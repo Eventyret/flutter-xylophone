f6beafb94b98157d856c3926d00edfc6e540bd17
Simen Daehlin <simen.daehlin@cads.co.uk>
2019-05-19 17:50:16 +0100

    Added all flatButtons this needs a cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -8,7 +9,67 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Center(
+              child: Column(
+            children: <Widget>[
+              FlatButton(
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note1.wav');
+                },
+                child: Text('Click Me'),
+                color: Colors.red,
+              ),
+              FlatButton(
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note2.wav');
+                },
+                child: Text('Click Me'),
+                color: Colors.orange,
+              ),
+              FlatButton(
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note3.wav');
+                },
+                child: Text('Click Me'),
+                color: Colors.yellow,
+              ),
+              FlatButton(
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note4.wav');
+                },
+                child: Text('Click Me'),
+                color: Colors.green,
+              ),
+              FlatButton(
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note5.wav');
+                },
+                child: Text('Click Me'),
+                color: Colors.teal,
+              ),
+              FlatButton(
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note6.wav');
+                },
+                child: Text('Click Me'),
+                color: Colors.blue,
+              ),
+              FlatButton(
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note7.wav');
+                },
+                child: Text('Click Me'),
+                color: Colors.purple,
+              ),
+            ],
+          )),
         ),
       ),
     );
